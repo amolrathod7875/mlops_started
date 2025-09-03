f411f08632294cdf4a46acf5e4d9f4430d52db1b
amolrathod7875 <amolrathod7875470402@gmail.com>
2025-09-03 16:06:37 +0530

    Streamlit Error solved

--- streamlit.py
+++ streamlit.py
@@ -12,7 +12,7 @@ hours = st.number_input("Hours studied :",min_value=0.0, step=1.0)
 if st.button("Predict"):
     try:
         data = [[hours]]
-        scaler_data = scaler.trandform(data)
+        scaler_data = scaler.transform(data)
         prediction = model.predict(scaler_data)
         st.write(f"Predicted Test Score : {prediction[0]:.2f}")
     except Exception as e:
