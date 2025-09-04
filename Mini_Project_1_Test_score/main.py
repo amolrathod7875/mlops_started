87cd313bc5c538a956c8c08d91d91adb3e36eebe
amolrathod7875 <amolrathod7875470402@gmail.com>
2025-09-04 17:15:01 +0530

    Logging And Error Handling

--- Mini_Project_1_Test_score/main.py
+++ Mini_Project_1_Test_score/main.py
@@ -3,6 +3,8 @@ import pandas as pd
 import numpy as np 
 from pydantic import BaseModel
 from fastapi import FastAPI
+from fastapi import HTTPException
+import logging
 
 model = None
 scaler = None
@@ -11,9 +13,15 @@ app = FastAPI()
 
 @app.on_event("startup")
 async def load_modal():
-    global model,scaler
-    model = joblib.load('LinearRegression_model.pkl')
-    scaler = joblib.load('scaler.pkl')
+    try:
+        global model,scaler
+        model = joblib.load('LinearRegression_model.pkl')
+        scaler = joblib.load('scaler.pkl')
+        logging.info('Model And Scaler have been Loaded Successfully')
+
+    except Exception as e:
+        logging.error(f"Error Loading model or scaler:{e}")
+        raise HTTPException(status_code = 500, detail="Error loading model")
 
 class PredictionRequests(BaseModel):
     hours_studied: float
@@ -26,10 +34,21 @@ def home():
 
 @app.post("/predict")
 def predict(request:PredictionRequests):
+    if model is None or scaler is None:
+        logging.info("Model is not loaded")
+        raise HTTPException(status_code = 503, detail="Model not loaded, please try again later")
+    
+    if (request.hours_studied <= 0):
+        raise HTTPException(status_code = 400, detail="Hours studied should be positive")
     hours = request.hours_studied
     data = pd.DataFrame([[hours]],columns=['Hours_Studied'])
     scaled_data = scaler.transform(data)
-    prediction = model.predict(scaled_data)
+    try:
+        prediction = model.predict(scaled_data)
+        logging.info(f"Prediction for {hours} hours : {prediction[0]}")
+    except Exception as e:
+        logging.error(f"Error During Prediction : {e}")
+        raise HTTPException(status_code = 500, detail="Error during prediction")
     return{
         "Predicted Test Score ":prediction[0]
     }
